616155b8fbd0d5767f2ebed5b5c9f0f721da778f
Filipe Augusto <filipeaugusto2130@gmail.com>
2022-10-28 20:22:41 -0300

    Criando uma Exception

--- src/main/java/one/digitalinnovation/parking/service/ParkingService.java
+++ src/main/java/one/digitalinnovation/parking/service/ParkingService.java
@@ -1,20 +1,28 @@
 package one.digitalinnovation.parking.service;
 
+import one.digitalinnovation.parking.exception.ParkingNotFoundException;
 import one.digitalinnovation.parking.model.Parking;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
 public class ParkingService {
 
-    private static Map<String, Parking> parkingMap = new HashMap<>();
+    private static final Map<String, Parking> parkingMap = new HashMap<>();
 
     static {
         var id = getUUID();
+        var id2 = getUUID();
         Parking parking = new Parking(id, "HFJ-8358", "RJ", "CORSA", "AMARELO");
+        Parking parking2 = new Parking(id2, "GDK-5385", "SP", "VECTRA", "CINZA");
         parkingMap.put(id, parking);
+        parkingMap.put(id2, parking2);
     }
 
     private static String getUUID() {
@@ -23,5 +31,21 @@ public class ParkingService {
 
     public List<Parking> findAll() {
         return parkingMap.values().stream().collect(Collectors.toList());
+    }
+
+    public Parking findById(String id) {
+        Parking parking = parkingMap.get(id);
+
+        if (parking == null) throw new ParkingNotFoundException(id);
+
+        return parking;
+    }
+
+    public Parking create(Parking parking) {
+        String uuid = getUUID();
+        parking.setId(uuid);
+        parking.setEntryDate(LocalDateTime.now());
+        parkingMap.put(uuid, parking);
+        return parking;
     }
 }
