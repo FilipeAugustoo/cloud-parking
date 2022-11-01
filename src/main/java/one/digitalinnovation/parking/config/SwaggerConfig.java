a540d768c520d2cf16cdb4b7acdf1e1787ab260f
Filipe Augusto <filipeaugusto2130@gmail.com>
2022-11-01 18:14:42 -0300

    Configurando o spring security no swagger

--- src/main/java/one/digitalinnovation/parking/config/SwaggerConfig.java
+++ src/main/java/one/digitalinnovation/parking/config/SwaggerConfig.java
@@ -5,11 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 
@@ -50,9 +53,25 @@ public class SwaggerConfig {
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(this.informacoesApi().build())
+                .securityContexts(Collections.singletonList(getSecurityContext()))
+                .securitySchemes(List.of(basicAuthScheme()))
                 .consumes(new HashSet<String>(List.of("application/json")))
                 .produces(new HashSet<String>(List.of("application/json")));
 
         return docket;
+    }
+
+    private SecurityContext getSecurityContext() {
+        return SecurityContext.builder()
+                .securityReferences(List.of(basicAuthReference()))
+                .build();
+    }
+
+    private SecurityReference basicAuthReference() {
+        return new SecurityReference("basicAuth", new AuthorizationScope[0]);
+    }
+
+    private SecurityScheme basicAuthScheme() {
+        return new BasicAuth("basicAuth");
     }
 }
