5bfaf5363648173dc0dd2b56a2f4838d15ac1a5e
Filipe Augusto <filipeaugusto2130@gmail.com>
2022-11-02 13:10:45 -0300

    Removendo Código não utilizado

--- src/main/java/one/digitalinnovation/parking/model/Parking.java
+++ src/main/java/one/digitalinnovation/parking/model/Parking.java
@@ -16,6 +16,7 @@ public class Parking {
 
     @Id
     private String id;
+
     private String license;
     private String state;
     private String model;
@@ -23,12 +24,4 @@ public class Parking {
     private LocalDateTime entryDate;
     private LocalDateTime exitDate;
     private Double bill;
-
-    public Parking(String id, String license, String state, String model, String color) {
-        this.id = id;
-        this.license = license;
-        this.state = state;
-        this.model = model;
-        this.color = color;
-    }
 }
