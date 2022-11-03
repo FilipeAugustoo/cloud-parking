6d0170a8593b47d9799a93ada1c51c5c397e40e5
Filipe Augusto <filipeaugusto2130@gmail.com>
2022-11-03 08:35:31 -0300

    Validando os DTO's

--- src/main/java/one/digitalinnovation/parking/controller/dto/ParkingCreateDTO.java
+++ src/main/java/one/digitalinnovation/parking/controller/dto/ParkingCreateDTO.java
@@ -3,12 +3,25 @@ package one.digitalinnovation.parking.controller.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 public class ParkingCreateDTO {
 
+    @Pattern(regexp = "^[a-zA-Z]{3}\\-\\d{4}$", message = "Placa deve ser no formato: '000-AAAA'")
+    @NotBlank
     private String license;
+
+    @NotBlank
+    @Size(max = 2, message = "Estado deve ser no formato: 'AA'")
     private String state;
+
+    @NotBlank
     private String model;
+
+    @NotBlank
     private String color;
 }
--- src/main/java/one/digitalinnovation/parking/controller/dto/UserCreateDTO.java
+++ src/main/java/one/digitalinnovation/parking/controller/dto/UserCreateDTO.java
@@ -3,11 +3,16 @@ package one.digitalinnovation.parking.controller.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Pattern;
+
 @Getter
 @Setter
 public class UserCreateDTO {
 
     private String name;
+
+    @Pattern(regexp = "^[a-z0-9_]+$", message = "Username deve ser em letras minúsculas")
     private String username;
+
     private String password;
 }
