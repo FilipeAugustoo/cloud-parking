4a9858280832be6003a5867ee27a133299cd50fa
Filipe Augusto <filipeaugusto2130@gmail.com>
2022-11-04 19:51:12 -0300

    Adicionando validaçoes

--- src/main/java/one/digitalinnovation/parking/controller/dto/CarCreateDTO.java
+++ src/main/java/one/digitalinnovation/parking/controller/dto/CarCreateDTO.java
@@ -3,12 +3,24 @@ package one.digitalinnovation.parking.controller.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 @Getter
 @Setter
 public class CarCreateDTO {
 
+
+    @Pattern(regexp = "^[a-zA-Z]{3}\\-\\d{4}$", message = "Placa deve ser no formato: '000-AAAA'")
+    @NotBlank(message = "License é obrigatório")
     private String license;
+
+    @NotBlank(message = "State é obrigatório")
     private String state;
+
+    @NotBlank(message = "Model é obrigatório")
     private String model;
+    
+    @NotBlank(message = "Color é obrigatório")
     private String color;
 }
