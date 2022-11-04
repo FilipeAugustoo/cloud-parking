4d81fbb963705d9040c744bacf674a38fe2eccc0
Filipe Augusto <filipeaugusto2130@gmail.com>
2022-11-04 19:49:11 -0300

    Adicionando novas colunas

--- src/main/java/one/digitalinnovation/parking/model/Car.java
+++ src/main/java/one/digitalinnovation/parking/model/Car.java
@@ -1,6 +1,7 @@
 package one.digitalinnovation.parking.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,21 +9,24 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.validation.constraints.Pattern;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @Entity
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Car {
 
     @Id
-    @Pattern(regexp = "^[a-zA-Z]{3}\\-\\d{4}$", message = "Placa deve ser no formato: '000-AAAA'")
     private String license;
 
     private String state;
     private String model;
+
+
     private String color;
+    private Boolean estaEstacionado;
+    private Integer quantEstacionou;
     private LocalDateTime entryDate;
     private LocalDateTime exitDate;
     private Double bill;
@@ -30,5 +34,15 @@ public class Car {
     @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
     private Parking parking;
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "license='" + license + '\'' +
+                ", state='" + state + '\'' +
+                ", model='" + model + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
 
 }
